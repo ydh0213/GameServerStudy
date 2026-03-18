f5967c1878109fdfd3c0c67469dcbbd72e37542a
윤대현 <ydh0213@gmail.com>
2026-03-18 18:34:55 +0900

    과제 18: backlog queue 최대치 구하기 수정 - 클라에서 동적 포트 고갈을 막기 위해 적당한 개수만큼 connect하고 바로 closesocket을 한 뒤 120초 이상 기다렸다가 다시 적당한 개수만큼 연결 및 해제를 반복 - 서버에서 백로그 큐가 꽉찰 때까지(65535개) 기다렸다가 전부 accept 하기 (그 때 NP풀 메모리 확인)

--- Assignments/18_BacklogQueueMaximum/Client/main.cpp
+++ Assignments/18_BacklogQueueMaximum/Client/main.cpp
@@ -2,11 +2,14 @@
 #include <winsock2.h>
 #include <ws2tcpip.h>
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
 const char* SERVER_IP = "127.0.0.1";
 constexpr int SERVER_PORT = 8888;
+constexpr int BATCH_NUMBER = 14'000;
+constexpr int WAIT_MS = 125'000;
 
 int main()
 {
@@ -27,33 +30,58 @@ int main()
 		return -1;
 	}
 
-	int connectedCount = 0;
+	int totalConnectedCount = 0;
+	bool isQueueFull = false; // 백로그 큐가 꽉 찼는지
 
-	cout << "연결 폭격을 시작합니다...\n";
+	cout << "연결 폭격을 시작합니다 (14,000개 배치 및 125초 대기 모드)...\n";
 
-	while (true)
+	while (!isQueueFull)
 	{
-		SOCKET sock = socket(AF_INET, SOCK_STREAM, 0);
+		vector<SOCKET> sockets;
+		cout << "\n[배치 시작] 새로운 14,000개 연결을 시도합니다...\n";
 
-		if (sock == INVALID_SOCKET)
+		for (int i = 0; i < BATCH_NUMBER; ++i)
 		{
-			cerr << "소켓 생성 실패! Error: " << WSAGetLastError() << '\n';
-			continue;
+			SOCKET sock = socket(AF_INET, SOCK_STREAM, 0);
+			if (sock == INVALID_SOCKET)
+			{
+				cerr << "소켓 생성 실패! Error: " << WSAGetLastError() << '\n';
+				continue;
+			}
+
+			if (connect(sock, (sockaddr*)&serverAddr, sizeof serverAddr) == SOCKET_ERROR)
+			{
+				// 에러 발생: 큐가 꽉 찼거나(10061/10060) 버퍼가 부족함(10055)
+				cerr << "\n[!] 연결 실패! 큐가 꽉 찼습니다. Error: " << WSAGetLastError() << '\n';
+				closesocket(sock);
+				isQueueFull = true;
+				break;
+			}
+
+			sockets.emplace_back(sock);
+
+			if (++totalConnectedCount % 1000 == 0)
+				cout << totalConnectedCount << "번째 연결 큐에 진입...\n";
 		}
 
-		if (connect(sock, (sockaddr*)&serverAddr, sizeof serverAddr) == SOCKET_ERROR)
-		{
-			cerr << "\n[!] 연결 실패! 큐가 꽉 찼거나 타임아웃 발생. Error: " << WSAGetLastError() << '\n';
-			cout << "총 성공한 연결 횟수 (큐에 들어간 개수): " << connectedCount << "\n";
+		// 이번 배치에서 연결했던 모든 소켓을 closesocket() 하여 FIN 패킷 전송 (서버는 CLOSE_WAIT 상태로 전환)
+		cout << "\n[정리 작업] 이번 배치에서 연결된 " << sockets.size() << "개의 소켓을 닫습니다...\n";
+		for (SOCKET sock : sockets)
+			closesocket(sock);
+
+		sockets.clear();
+
+		if (isQueueFull)
 			break;
-		}
 
-		connectedCount++;
-
-		// 진행 상황 표시 (100번마다)
-		if (connectedCount % 100 == 0)
-			cout << connectedCount << "번째 연결 큐에 진입...\n";
+		// closesocket() 후 FINE_WAIT_2 상태로 진입하여 기본값 120초 이상 지나서 포트가 완전히 해제되길 기다림
+		cout << "[휴식] 포트 고갈(10055) 방지를 위해 125초간 대기합니다...\n";
+		Sleep(WAIT_MS);
 	}
+
+	cout << "\n======================================\n";
+	cout << "테스트 종료! 총 성공한 연결 횟수: " << totalConnectedCount << "\n";
+	cout << "======================================\n";
 
 	Sleep(INFINITE); // 연결 폭격이 끝난 후 프로그램이 종료되지 않도록 대기
 	WSACleanup();
